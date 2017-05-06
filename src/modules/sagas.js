118bb6c093d79ccb17e8142c489b8a6caae581b2
Nicolas Djambazian <nicolas@djambazian.fr>
2017-05-06 15:15:08 +0200

    Install Redux-saga

---
+++ src/modules/sagas.js
@@ -0,0 +1,4 @@
+// @flow
+export default function* rootSaga(): Generator<*, *, *> {
+  yield [];
+}
--- src/modules/store.js
+++ src/modules/store.js
@@ -1,18 +1,23 @@
 // @flow
 
 import { applyMiddleware, createStore, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+
 import dismissKeyboard from './middlewares/dissmissKeyboard';
 
 import reducers from './reducers';
+import rootSaga from './sagas';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const sagaMiddleware = createSagaMiddleware();
 
 export default (callback: Function) => {
-  const middlewares = [dismissKeyboard];
+  const middlewares = [dismissKeyboard, sagaMiddleware];
 
   const enhancers = [applyMiddleware(...middlewares)];
 
   const store = createStore(reducers, composeEnhancers(...enhancers));
+  sagaMiddleware.run(rootSaga);
 
   callback(store);
   return store;
