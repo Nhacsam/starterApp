de136760da108377dbaf3322396d2aba8ca88845
Nicolas Djambazian <nicolas@djambazian.fr>
2017-05-25 14:22:19 +0200

    Fix jest

--- src/modules/__tests__/User.js
+++ src/modules/__tests__/User.js
@@ -1,11 +1,6 @@
-import {
-  register,
-  registerSuccess,
-  registerFail,
-  registeringSelector,
-  registeringFailureSelector,
-  userReducer,
-} from '../User';
+import { register, registeringSelector, registeringFailureSelector, userReducer } from '../User';
+
+import { createSuccess, createFailure } from '../Model/User';
 
 describe('User module', () => {
   const user = { email: 'test@test.com' };
@@ -14,90 +9,60 @@ describe('User module', () => {
     it('should create a register action', () => {
       expect(register(user)).toEqual({
         type: 'USER.REGISTER',
-        user,
-      });
-    });
-
-    it('should create a register success action', () => {
-      expect(registerSuccess(user)).toEqual({
-        type: 'USER.REGISTER_SUCCESS',
-        user,
-      });
-    });
-
-    it('should create a register fail action', () => {
-      expect(registerFail()).toEqual({
-        type: 'USER.REGISTER_FAILURE',
+        payload: { user },
       });
     });
   });
 
   describe('selectors', () => {
     it('should select the registerging boolean', () => {
-      const state = { user: { _meta: { registering: false } } };
+      const state = { user: { registering: false } };
       expect(registeringSelector(state)).toBe(false);
-      state.user._meta.registering = true;
+      state.user.registering = true;
       expect(registeringSelector(state)).toBe(true);
     });
 
     it('should select the registerging failure boolean', () => {
-      const state = { user: { _meta: { registerFailure: false } } };
+      const state = { user: { registerFailure: false } };
       expect(registeringFailureSelector(state)).toBe(false);
-      state.user._meta.registerFailure = true;
+      state.user.registerFailure = true;
       expect(registeringFailureSelector(state)).toBe(true);
     });
   });
 
   describe('reducer', () => {
-    const randomState = {
-      ...user,
-      _meta: {
-        registering: false,
-        registerFailure: false,
-      },
+    const initialState = {
+      registering: false,
+      registerFailure: false,
     };
 
     it('should return an initial state', () => {
-      expect(userReducer()).toEqual({
-        _meta: {
-          registering: false,
-          registerFailure: false,
-        },
-      });
+      expect(userReducer(undefined, { type: '' })).toEqual(initialState);
     });
 
     it('should return the last state by default', () => {
-      expect(userReducer(randomState, { type: 'UNKNOWN' })).toBe(randomState);
+      expect(userReducer(initialState, { type: 'UNKNOWN' })).toBe(initialState);
     });
 
     it('should save the user and mark as registering', () => {
-      expect(userReducer(randomState, register(user))).toEqual({
-        ...user,
-        _meta: {
-          registering: true,
-          registerFailure: false,
-        },
+      expect(userReducer(initialState, register(user))).toEqual({
+        registering: true,
+        registerFailure: false,
       });
     });
 
     it('should save the user and stop registering', () => {
-      const state = userReducer(randomState, register({ email: 'other_email' }));
-      expect(userReducer(state, registerSuccess(user))).toEqual({
-        ...user,
-        _meta: {
-          registering: false,
-          registerFailure: false,
-        },
+      const state = userReducer(initialState, register({ email: 'other_email' }));
+      expect(userReducer(state, createSuccess(user))).toEqual({
+        registering: false,
+        registerFailure: false,
       });
     });
     it('should mark registering as error and stop it', () => {
-      const state = userReducer(randomState, register({ email: 'other_email' }));
-      expect(userReducer(state, registerFail())).toEqual({
-        email: 'other_email',
-        _meta: {
-          registering: false,
-          registerFailure: true,
-        },
+      const state = userReducer(initialState, register({ email: 'other_email' }));
+      expect(userReducer(state, createFailure())).toEqual({
+        registering: false,
+        registerFailure: true,
       });
     });
   });
